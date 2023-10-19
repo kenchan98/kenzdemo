60dfcfaf387c9f25f8f0e10ddab48588aea36ec8
Ken chan <kchan27@ford.com>
2023-10-19 19:58:41 +0100

    removed AppContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/camera.css";
 import "@/styles/home.css";
-import AppContext from "./AppContext";
+//import AppContext from "./AppContext";
 import { useState } from "react";
 export default function App({ Component, pageProps }) {
   const [vehicleData, setVehicleData] = useState({ imgData: "test 1" });
