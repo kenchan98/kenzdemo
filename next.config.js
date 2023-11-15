1447d3d450aeef1d6d6c612fc52897bc5318026c
Ken chan <kchan27@ford.com>
2023-11-15 00:50:02 +0000

    updated PWA related file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
+const nextConfig = {};
 module.exports = nextConfig;
 
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  //disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
